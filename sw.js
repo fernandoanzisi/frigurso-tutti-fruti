5966e8706386388939a5a81d7c26cc6e2119486a
Fernando Anzisi <roccoanzisi2009@gmail.com>
2026-08-17 15:56:04 -0300

    Primera version: Frigurso Tutti Fruti (local + online por codigo de sala)

---
+++ sw.js
@@ -0,0 +1,35 @@
+const CACHE = 'tuttifruti-v1';
+const PRECACHE = ['./', './index.html', './manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c =>
+      Promise.all(PRECACHE.map(url => c.add(url).catch(() => {})))
+    )
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('message', e => {
+  if (e.data?.type === 'SKIP_WAITING') self.skipWaiting();
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      const fresh = fetch(e.request).then(res => {
+        if (res.ok) caches.open(CACHE).then(c => c.put(e.request, res.clone()));
+        return res;
+      }).catch(() => cached);
+      return cached || fresh;
+    })
+  );
+});
